05ca1fdca613f9062590bc724cd4181cb71b2c04
saifurjob <saifurjob@gmail.com>
2015-08-09 01:02:05 +0600

    This is my full work of theoritical Java

---
+++ src/java_regular_expression/Matching_dash_plus_group.java
@@ -0,0 +1,41 @@
+
+package java_regular_expression;
+
+public class Matching_dash_plus_group
+{
+    public static void main(String[] args)
+    {
+        String s = "h";
+        
+        System.out.println("checks any charecters");
+        String pattern3 = "[a-z]"; // checks any charecters
+        System.out.println(s.matches(pattern3)); //true
+        System.out.println("A".matches(pattern3)); //false
+        System.out.println("2".matches(pattern3)); //false
+        System.out.println("");
+        
+        System.out.println("hello".matches(pattern3)); //false
+        System.out.println("HELLO".matches(pattern3)); //false
+        System.out.println("222".matches(pattern3)); //false
+        System.out.println("");
+        
+        System.out.println("hello".matches("[a-z]+")); //true
+        System.out.println("HELLO".matches("[A-Z]+")); //true
+        System.out.println("222".matches("[0-9]+")); //true
+        System.out.println("");
+        
+        System.out.println("hello017".matches("[a-z]+")); //false
+        System.out.println("HELLOasd".matches("[A-Z]+")); //false
+        System.out.println("222aaa".matches("[0-9]+")); //false
+        System.out.println("");
+        
+        System.out.println("helloHELLO".matches("[a-zA-Z]+")); //true
+        System.out.println("HELLOasd".matches("[A-Z a-z]+")); //true
+        System.out.println("222aaa".matches("[0-9a-z]+")); //true
+        System.out.println("");
+        
+        System.out.println("555 hello HELLO".matches("[0-9 a-z A-Z]+")); //true
+        System.out.println("".matches("[0-9 a-z A-Z]+")); //false
+        System.out.println("".matches("[0-9 a-z A-Z]*")); //true
+    }
+}
